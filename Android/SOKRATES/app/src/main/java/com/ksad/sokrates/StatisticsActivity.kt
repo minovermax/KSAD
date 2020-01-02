03719815badf44df6cf097842de8d7d0b20baa3a
Soungmin Lee <soungminlee@gmail.com>
2020-01-01 23:27:25 -0500

    SOKRATES Android Migration

---
+++ Android/SOKRATES/app/src/main/java/com/ksad/sokrates/StatisticsActivity.kt
@@ -0,0 +1,12 @@
+package com.ksad.sokrates
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class StatisticsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_statistics)
+    }
+}
